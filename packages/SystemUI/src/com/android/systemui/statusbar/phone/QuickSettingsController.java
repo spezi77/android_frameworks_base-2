shaheerkv/android_frameworks_base

Framework: add compass tile (1/2)
Add a compass tile to quick settings.
Clicking on it enables the sensor and clicking again disables it.

Screenshot: http://goo.gl/2frdpy

Modified by liquid0624 to work with SlimRoms quick settings

Change-Id: Icd9d12929f0491f6958f30de139ef39bb4c2e037

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -27,6 +27,7 @@ import static com.android.internal.util.liquid.QSConstants.TILE_BLUETOOTH;
 import static com.android.internal.util.liquid.QSConstants.TILE_BRIGHTNESS;
 import static com.android.internal.util.liquid.QSConstants.TILE_BUGREPORT;
 import static com.android.internal.util.liquid.QSConstants.TILE_CAMERA;
+import static com.android.internal.util.liquid.QSConstants.TILE_COMPASS;
 import static com.android.internal.util.liquid.QSConstants.TILE_CONTACT;
 import static com.android.internal.util.liquid.QSConstants.TILE_CUSTOM;
 import static com.android.internal.util.liquid.QSConstants.TILE_CUSTOM_KEY;
@@ -85,6 +86,7 @@ import com.android.systemui.quicksettings.BluetoothTile;
 import com.android.systemui.quicksettings.BrightnessTile;
 import com.android.systemui.quicksettings.BugReportTile;
 import com.android.systemui.quicksettings.CameraTile;
+import com.android.systemui.quicksettings.CompassTile;
 import com.android.systemui.quicksettings.ContactTile;
 import com.android.systemui.quicksettings.CustomTile;
 import com.android.systemui.quicksettings.ExpandedDesktopTile;
@@ -225,6 +227,8 @@ public class QuickSettingsController {
                 qs = new BrightnessTile(mContext, this);
             } else if (tile.equals(TILE_CAMERA) && cameraSupported) {
             	qs = new CameraTile(mContext, this, mHandler);
+            } else if (tile.equals(TILE_COMPASS)) {
+                qs = new CompassTile(mContext, this);
             } else if (tile.equals(TILE_RINGER)) {
                 qs = new RingerModeTile(mContext, this);
             } else if (tile.equals(TILE_SYNC)) {

spezi77/android_frameworks_base-2

Signed-off-by: scott <scott@ubuntu>
Conflicts:
	core/java/com/android/internal/util/beanstalk/DeviceUtils.java
	packages/SystemUI/res/values/bam_arrays.xml
	packages/SystemUI/res/values/bs_strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -27,6 +27,7 @@ import static com.android.internal.util.beanstalk.QSConstants.TILE_BLUETOOTH;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_BRIGHTNESS;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_BUGREPORT;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_CAMERA;
+import static com.android.internal.util.beanstalk.QSConstants.TILE_COMPASS;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_CONTACT;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_CUSTOM;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_CUSTOM_KEY;
@@ -89,6 +90,7 @@ import com.android.systemui.quicksettings.BluetoothTile;
 import com.android.systemui.quicksettings.BrightnessTile;
 import com.android.systemui.quicksettings.BugReportTile;
 import com.android.systemui.quicksettings.CameraTile;
+import com.android.systemui.quicksettings.CompassTile;
 import com.android.systemui.quicksettings.ContactTile;
 import com.android.systemui.quicksettings.CustomTile;
 import com.android.systemui.quicksettings.ExpandedDesktopTile;
@@ -229,6 +231,8 @@ public class QuickSettingsController {
                 qs = new BrightnessTile(mContext, this);
             } else if (tile.equals(TILE_CAMERA) && cameraSupported) {
             	qs = new CameraTile(mContext, this, mHandler);
+            } else if (tile.equals(TILE_COMPASS)) {
+                qs = new CompassTile(mContext, this);
             } else if (tile.equals(TILE_RINGER)) {
                 qs = new RingerModeTile(mContext, this);
             } else if (tile.equals(TILE_SYNC)) {